b2467d32842cb471eb54ff65fdc595799d7ea7e0
Tiller <ctiller@proforma.com>
2018-03-27 15:02:05 -0400

    connect angular to application

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+const API_KEY = "770f66fa3ba5c6f2edb52bf531985f94";
+const SHARED_SECRET = "1261c812bbb8183b4d8fe3a65bd6b051";
 
-document.addEventListener('DOMContentLoaded', main)
+angular
+  .module("mainApplication", [])
+  .controller("mainController", ['$scope', '$http', ($scope, $http) => {
+    $scope.name = "Angular";
+  }]);
